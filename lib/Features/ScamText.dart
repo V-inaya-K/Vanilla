11a998278a82ecfc8522300486fda080a3678436
Vinayak Shukla <98159594+V-inaya-K@users.noreply.github.com>
2025-04-11 11:26:47 +0530

    Update ScamText.dart

--- lib/Features/ScamText.dart
+++ lib/Features/ScamText.dart
@@ -16,7 +16,7 @@ class ScamText extends StatefulWidget {
 class _ScamTextState extends State<ScamText> {
   final TextEditingController _messageController = TextEditingController();
   String _result = '';
-  final String apiKey = 'AIzaSyCrHyVnsBH7zsQMfjRZeK36AXhETEVMQP4';
+  final String apiKey = '{API KEY}';
 
   Future<void> _identifyFraudEmail() async {
     final String userInput = _messageController.text;
